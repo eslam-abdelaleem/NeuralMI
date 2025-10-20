70ec4949ef7dc368669d56f6917a68ddbb8010f7
Eslam Abdelaleem <eslamalaasabra1@gmail.com>
2025-10-20 12:56:26 -0400

    fix: Add docs dependencies to fix build

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="neural_mi",
-    version="1.0.0",
+    version="1.1.0",
     description="A toolbox for Mutual Information estimation in neural data.",
     packages=find_packages(),
     install_requires=[
